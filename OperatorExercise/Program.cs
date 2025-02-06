c9cd9ec906514ba2350afe27ee51bbef807adf21
Davonte <davonte.nyc@gmail.com>
2025-02-05 23:54:13 -0500

    Operator Exercise Progress

--- OperatorExercise/Program.cs
+++ OperatorExercise/Program.cs
@@ -4,7 +4,28 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int a = 17; 
+            int b = 4;
+
+            int sum = a + b;
+            int difference = a - b;
+            int product = a * b;
+
+            string resultMessage = $"Sum: {sum}, " + $"Difference: {difference}, " + $"Product: {product} ";
+            
+            Console.WriteLine(resultMessage);
+            
+            int quotient = a / b;
+            int remainder = a % b;
+            
+            string resultMessage2 = $"{a}/{b} is {quotient} remainder {remainder}";
+            
+            Console.WriteLine(resultMessage2);
+            
+            
+            //Waiting on 1-1 to Complete full exercise
         }
+        
+        static double AreaOfCircle
     }
 }
